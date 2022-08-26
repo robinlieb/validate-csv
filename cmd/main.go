4c39ad99775313bf19b478d79526e5ef07f91491
Robin Lieb <robin.j.lieb@gmail.com>
2022-08-26 18:58:18 +0200

    feat: added cli functionality

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	validateCmd := flag.NewFlagSet("validate", flag.ExitOnError)
+	filename := validateCmd.String("file", "", "Input file to validate")
+
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'validate' subcommand")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "validate":
+		validateCmd.Parse(os.Args[2:])
+		err := HandleValidate(*filename)
+		if err != nil {
+			fmt.Printf("Recieved error: %s\n", err)
+			os.Exit(1)
+		}
+	default:
+		fmt.Println("expected 'validate' subcommand")
+		os.Exit(1)
+	}
+}
+
+func HandleValidate(filename string) error {
+	return nil
 }
