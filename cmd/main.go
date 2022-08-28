c929cbbb60d27c0dcbae46daee157888986dad58
Robin Lieb <robin.j.lieb@gmail.com>
2022-08-28 21:00:45 +0200

    fix: command description

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	validateCmd := flag.NewFlagSet("validate", flag.ExitOnError)
-	filename := validateCmd.String("file", "", "Input file to validate. Should be in CSV format.")
+	filename := validateCmd.String("file", "", "Input file to validate. It should be in CSV format.")
 
 	if len(os.Args) < 2 {
 		fmt.Println("Expected 'validate' subcommand")
