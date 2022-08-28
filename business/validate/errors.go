a6855dd24bbf6f0e95cd1ba6489ef1c67404f8aa
Robin Lieb <robin.j.lieb@gmail.com>
2022-08-28 20:19:02 +0200

    fix: text of ErrSumsNotEqual

--- business/validate/errors.go
+++ business/validate/errors.go
@@ -8,4 +8,4 @@ import (
 var ErrDuplicateNumbers = errors.New("duplicate numbers provided in input")
 
 // ErrSumsNotEqual is the error used when sums of rows are not equal.
-var ErrSumsNotEqual = errors.New("sums of all rows not equal")
+var ErrSumsNotEqual = errors.New("sums of all rows are not equal")
