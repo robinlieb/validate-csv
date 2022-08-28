03c06af4244051b8b3b4caba6acf81169726569f
Robin Lieb <robin.j.lieb@gmail.com>
2022-08-28 22:07:13 +0200

    refactor: moved cli from main

--- cmd/main.go
+++ cmd/main.go
@@ -1,70 +1,13 @@
 package main
 
 import (
-	"encoding/csv"
-	"flag"
 	"fmt"
 	"os"
-	"strconv"
-
-	"github.com/robinlieb/validate-csv/business/validate"
 )
 
 func main() {
-	validateCmd := flag.NewFlagSet("validate", flag.ExitOnError)
-	filename := validateCmd.String("file", "", "Input file to validate. It should be in CSV format.")
-
-	if len(os.Args) < 2 {
-		fmt.Println("Expected 'validate' subcommand")
+	if err := CmdInit(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
-
-	switch os.Args[1] {
-	case "validate":
-		validateCmd.Parse(os.Args[2:])
-		err := HandleValidate(*filename)
-		if err != nil {
-			fmt.Printf("Recieved error: %s\n", err)
-			os.Exit(1)
-		}
-	default:
-		fmt.Println("Expected 'validate' subcommand")
-		os.Exit(1)
-	}
-}
-
-func HandleValidate(filename string) error {
-	file, err := os.Open(filename)
-	if err != nil {
-		return err
-	}
-
-	defer file.Close()
-
-	csvReader := csv.NewReader(file)
-	data, err := csvReader.ReadAll()
-	if err != nil {
-		return err
-	}
-
-	var mat [][]int
-
-	for _, row := range data {
-		var matRow []int
-		for _, value := range row {
-			i, err := strconv.Atoi(value)
-			if err != nil {
-				return err
-			}
-			matRow = append(matRow, i)
-		}
-		mat = append(mat, matRow)
-	}
-
-	err = validate.Validate(mat)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
